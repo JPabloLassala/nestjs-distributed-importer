7de3eab74b1e568e387d32d36ba4b086a4374d11
Juan Pablo Lassala <juanpablo.lassala@shopadvizor.com>
2021-07-17 16:27:48 -0300

    Install Knex

--- src/infrastructure/modules/app.module.ts
+++ src/infrastructure/modules/app.module.ts
@@ -1,13 +1,22 @@
-import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import config from '../config/config';
-import { AppService } from '../services/app.service';
-import databaseConfig from '../config/database.config';
-import { HealthModule } from 'src/domain/modules/health.module';
+import { ConfigModule } from '@nestjs/config';
+import { KnexModule } from 'nest-knexjs';
 import { AppController } from 'src/application/controllers/app.controller';
+import { HealthModule } from 'src/domain/modules/health.module';
+import config from '../config/config';
+import databaseConfig from '../config/database.config';
+import { AppService } from '../services/app.service';
 
 @Module({
   imports: [
+    KnexModule.forRoot({
+      config: {
+        client: 'sqlite3',
+        connection: {
+          filename: './db/chinook.db',
+        },
+      },
+    }),
     ConfigModule.forRoot({
       envFilePath: ['.env.local', '.env'],
       isGlobal: true,
