ddefb21f3e2942f40bbb7839775e89857318bd28
Juan Pablo Lassala <juanpablo.lassala@shopadvizor.com>
2021-07-28 00:11:33 -0300

    Add more test mockups

---
+++ __tests__/services/genre.service.spec.ts
@@ -0,0 +1,9 @@
+describe('AlbumService', () => {
+  describe('getters', () => {
+    it('Should return all genres');
+
+    it('Should return genres in band');
+
+    it('Should return song genre');
+  });
+});
---
+++ __tests__/services/track.service.spec.ts
@@ -0,0 +1,11 @@
+describe('TrackService', () => {
+  describe('getters', () => {
+    it('Should return all tracks');
+
+    it('Should return tracks in album');
+
+    it('Should return tracks from band');
+
+    it('Should return tracks from playlist');
+  });
+});
