99725c50ec4c4b65dded9d6f3b7272770e0e6b22
Juan Pablo Lassala <juanpablo.lassala@shopadvizor.com>
2021-07-29 00:25:30 -0300

    Add getFromAlbum to ArtistRepository

--- src/infrastructure/repositories/artist.repository.ts
+++ src/infrastructure/repositories/artist.repository.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { Knex } from 'knex';
 import { InjectModel } from 'nest-knexjs';
-import { Artist } from 'src/domain/interfaces';
+import { Album, Artist } from 'src/domain/interfaces';
 
 @Injectable()
 export class ArtistRepository {
   constructor(@InjectModel() private readonly knex: Knex) {}
 
   getBaseQuery(): Knex.QueryBuilder {
-    return this.knex.from({ a: 'artists' });
-  }
-
-  async findAll(): Promise<Artist[]> {
-    const artists = await this.getBaseQuery().select({
+    return this.knex.from({ a: 'artists' }).select({
       id: 'ArtistId',
       name: 'Name',
     });
+  }
+
+  async findAll(): Promise<Artist[]> {
+    const artists = await this.getBaseQuery();
 
     return artists;
+  }
+
+  async getFromAlbum(album: Album): Promise<Artist> {
+    return await this.getBaseQuery()
+      .leftJoin({ alb: 'albums' }, 'alb.ArtistId', 'a.ArtistId')
+      .where({
+        'alb.id': album.id,
+      });
   }
 }
